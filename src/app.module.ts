b5368c072030aa57695a6c9d35197406d3ea643d
AtilaRocha <atila.vini@hotmail.com>
2025-06-11 10:52:53 -0300

    wip/create DDD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { ClientesModule } from './clientes/clientes.module';
+import { ProdutosModule } from './produtos/produtos.module';
+import { PedidosModule } from './pedidos/pedidos.module';
+import { RelatoriosModule } from './relatorios/relatorios.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UsuariosModule, ClientesModule, ProdutosModule, PedidosModule, RelatoriosModule],
   controllers: [AppController],
   providers: [AppService],
 })
